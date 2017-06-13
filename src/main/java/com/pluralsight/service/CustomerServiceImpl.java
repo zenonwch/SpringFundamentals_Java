a031413ff310e8ebc09944009abf96384a38a118
Andrey Veshtard <andrey.veshtard@ctco.lv>
2017-06-13 15:20:50 +0300

    scope = prototype

--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -3,13 +3,14 @@ package com.pluralsight.service;
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service("customerService")
-@Scope("singleton")
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class CustomerServiceImpl implements CustomerService {
 
 //	@Autowired                                                                                                  // member injection
